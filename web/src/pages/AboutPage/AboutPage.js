97c94a6c801670bc202ff4f10c631642fa1e6460
Braden Douglass <braden.douglass@gmail.com>
2020-04-13 14:07:21 -0400

    Blog Post Pages.
    Generate all needed items to be able to display a blog post, blog posts.
    Along with routes, DB changes, etc.

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,24 +1,10 @@
-import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout'
 
 const AboutPage = () => {
   return (
-    <>
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li><Link to={routes.about()}>About</Link></li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <p>
-          This site created to test the functionality behind Redwood js.
-        </p>
-
-        <Link to={routes.home()}>Home</Link>
-      </main>
-    </>
+    <BlogLayout>
+      <p>This site created to test the functionality behind Redwood js.</p>
+    </BlogLayout>
   )
 }
 
