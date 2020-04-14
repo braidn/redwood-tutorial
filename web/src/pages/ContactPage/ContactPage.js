d3267d737bdcb0463d4cf23cf722b45192571932
Braden Douglass <braden.douglass@gmail.com>
2020-04-14 07:32:21 -0400

    Finish Contact Page.
    This completes the lifecycle of the contact page and really shows off
    how Redwood has a pretty solid form solution. Also flipping through some
    of the generated schema files for Prisma2, this whole library is looking
    much better.

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -4,18 +4,45 @@ import {
   TextField,
   TextAreaField,
   FieldError,
+  FormError,
   Label,
   Submit,
 } from '@redwoodjs/web'
+import { useMutation } from '@redwoodjs/web'
+import { useForm } from 'react-hook-form'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: ContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
 
 const ContactPage = () => {
+  const formMethods = useForm({ mode: 'onBlur' })
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onComplete: () => {
+      alert('YOU DID IT')
+      formMethods.reset()
+    }
+  })
+
   const onSubmit = (data) => {
+    create({ variables: { input: data } })
     console.log(data)
   }
 
   return (
     <BlogLayout>
-      <Form onSubmit={onSubmit} validation={{ mode: 'onBlur' }}>
+      <Form
+        onSubmit={onSubmit}
+        formMethods={formMethods}
+      >
+        <FormError
+          error={error}
+          wrapperStyle={{ color: 'red', backgroundColor: 'lavenderblush' }}
+        />
         <Label
           name="name"
           style={{ display: 'block' }}
@@ -63,7 +90,10 @@ const ContactPage = () => {
           validation={{ required: true }}
         />
         <FieldError name="message" style={{ color: 'red' }} />
-        <Submit style={{ display: 'block' }}>Save</Submit>
+        <Submit
+          style={{ display: 'block' }}
+          disabled={loading}
+        >Save</Submit>
       </Form>
     </BlogLayout>
   )
